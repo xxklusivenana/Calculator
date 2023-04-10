119a044df8613eb6349e41d97766b9882ffb5c38
JaayEssencee <janiya.green2005@gmail.com>
2023-04-10 11:19:52 -0400

    123

---
+++ calculator.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+
+#JaayEssencee
+
+#displays a black, resizeable screen
+root = tk.Tk()
+root.title("Calculator Project: Intro To CompSci")
+root.config(bg = "black")
+root.geometry("600x400")
+root.resizable(True, True)
+
+root.mainloop()
+#
