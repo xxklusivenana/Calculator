0c588b04ae4eec10d695da2217265f27b462410f
JaayEssencee <janiya.green2005@gmail.com>
2023-04-10 11:50:34 -0400

    abd

--- calculator.py
+++ calculator.py
@@ -8,6 +8,15 @@ root.title("Calculator Project: Intro To CompSci")
 root.config(bg = "black")
 root.geometry("600x400")
 root.resizable(True, True)
-
-root.mainloop()
 #
+label = tk.Label(root, text = "Jaay's Calc", bg = "black")
+label.pack(anchor = tk.W, expand = True, fill = tk.BOTH)
+
+from tkinter import ttk
+frame = ttk.Frame(root, width = 400, height = 200, border = 6, relief = "sunken")
+frame.pack(anchor = tk.W, expand = True, fill = tk.BOTH)
+#buttons
+percentButton = tk.Button(frame, text = "%", font = ("Georgia", 14),width = 20, height = 10)
+percentButton.grid(row = 0, column = 0, sticky = "w")
+#
+root.mainloop()
